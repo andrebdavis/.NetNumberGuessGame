be0af7e9b0a3995cd03adacdfe00ed14c52a5249
andrebdavis <andreb.davis2@gmail.com>
2024-03-14 22:17:07 -0400

    Game going good should be done

--- NumberGuessGame.cs/NmberGuessGame.cs
+++ NumberGuessGame.cs/NmberGuessGame.cs
@@ -8,6 +8,7 @@ class Program
         int randomNumber = random.Next(1, 100);
 
         Console.WriteLine("Welcome to the Number Guessing Game!");
+        
         Console.WriteLine("Ive picked a random number between 1 and 100. Try to guess it!");
 
         int guess = 0;
@@ -25,9 +26,14 @@ class Program
                 continue; // loop to propmt the user again
 
             }
+            //check if the guess is correct
             else if (guess > randomNumber)
             {
-                
+                Console.WriteLine("Too low! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the correct number: " + randomNumber);
             }
 
 
